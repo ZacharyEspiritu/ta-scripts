4d142a9bac888729a588d794b9c64169f81d3b1d
Zachary Espiritu <me@zacharyespiritu.com>
2019-03-22 19:10:11 -0400

    Add script for converting @brown.edu emails to CS logins

---
+++ student-id-management/emails-to-logins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Consumes a list of @brown.edu emails from STDIN. Returns the corresponding
+# CS logins for each of those email addresses.
+#
+# Author: zespirit
+# Last modified: 03/22/2019
+
+while read STUDENT_EMAIL; do
+    local STUDENT_LOGIN=$(ldapsearch mail="$STUDENT_EMAIL" -Q | \
+                          grep "cn: " | \
+                          cut -f2 -d " ")
+    echo $STUDENT_LOGIN
+done
