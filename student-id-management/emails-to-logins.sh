391ef506804f30ba6bad0f70869debbdffbba621
Zachary Espiritu <me@zacharyespiritu.com>
2019-03-22 19:45:09 -0400

    Update emails-to-logins.sh

--- student-id-management/emails-to-logins.sh
+++ student-id-management/emails-to-logins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Consumes a list of @brown.edu emails from STDIN. Returns the corresponding
-# CS logins for each of those email addresses.
+# Consumes a list of @brown.edu emails from STDIN (each email should be separated
+# by a newline). Returns the corresponding CS logins for each of those email 
+# addresses.
 #
 # Author: zespirit
 # Last modified: 03/22/2019
