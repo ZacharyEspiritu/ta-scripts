98b3ae20b6485cac3e50ea58ae456107e1e4f4be
Zachary Espiritu <me@zacharyespiritu.com>
2019-03-23 09:55:20 -0400

    Clean up create-student-directories.sh

--- protected-student-directories/create-student-directories.sh
+++ protected-student-directories/create-student-directories.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Expects list of student logins on stdin. Creates student directories for each
+# Expects list of student logins on STDIN. Creates student directories for each
 # student such that they are the only student able to enter that directory.
+#
+# Make sure to update the BASE_DIR variable marked with the "MODIFY" keyword to
+# specify the directory that your student directories should be made in.
+#
+# You might also want to customize the permissions given to each student directory
+# by modifying the permissions set in the `setfacl` command called from the
+# `make_student_dir` function.
 #
 # Written by: zespirit
 # Last modified on 02/20/2019
 
 # MODIFY: The directory to make the student directories in.
-BASE_DIR="/course/cs1660/student"
+BASE_DIR="/course/csXXXX/student"
 
 # Usage: make_student_dir <login>
 make_student_dir() {
-	STUDENT_DIR="$BASE_DIR/$1"
+    local STUDENT_DIR="$BASE_DIR/$1"
 
     if [ -d $STUDENT_DIR ]; then
-        echo "WARNING: $1 already had a directory; skipping..."
+        echo "WARNING: $1 already had a directory; skipping..." >&2
     else
         mkdir $STUDENT_DIR
         chmod ug=rwx,o= $STUDENT_DIR
@@ -24,7 +31,8 @@ make_student_dir() {
 
 # Check that the /student directory exists:
 if [ ! -d $BASE_DIR ]; then
-    echo "ERROR: The $BASE_DIR directory doesn't exist. Create the directory manually first."
+    echo "ERROR: The $BASE_DIR directory doesn't exist. Create the " >&2
+    echo "directory manually first." >&2
     exit 1
 fi
 
