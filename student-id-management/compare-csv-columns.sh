cbb92385f0d26b942651a92ed643c010825aa6e7
Zachary Espiritu <me@zacharyespiritu.com>
2019-04-13 15:29:18 -0400

    Update compare-csv-columns.sh

--- student-id-management/compare-csv-columns.sh
+++ student-id-management/compare-csv-columns.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# Compares two columns of two different (or perhaps, the same) CSVs; returns
+# all lines in the right CSV's column that are not in the left CSV's column.
+#
+# You might find this script helpful for form-related checks such as "who
+# didn't fill out a form?" or "who didn't hand in?". (This is essentially
+# a more generalized version of the 'xor-student-lists.sh' script.)
+#
 # Author: zespirit
-# Last modified: 04/12/2019
+# Last modified: 04/13/2019
 
 # Parse command-line arguments:
 PARAMS=""
 while (( "$#" )); do
     case "$1" in
         -h|--help)
-            echo "$0 --left-csv <name> --right-csv <name> --left-col <num> --right-col <num>"
+	    echo "Returns all values in the specified column in the 'right' CSV that"
+	    echo "are not in the specified column in the 'left' CSV."
+            echo "Usage: $0 [--left-csv <name>] [--right-csv <name>] "
+	    echo "          [--left-col <num>] [--right-col <num>]"
+	    echo "Flags:"
+	    echo "  [-l | --left-csv]:   The CSV on the 'left'."
+	    echo "  [-r | --right-csv]:  The CSV on the 'right'."
+	    echo "  [-lc | --left-col]:  The column number in the CSV on the 'left' to compare."
+	    echo "  [-rc | --right-col]: The column number in the CSV on the 'left' to compare."
             exit 0
             ;;
         -l|--left-csv)
@@ -46,36 +61,37 @@ while (( "$#" )); do
     esac
 done
 
+# Validate command-line arguments:
 if [ -z "$LEFT_CSV_PATH" ]; then
-	echo "ERROR: Left *.csv not specified." >&2
+	echo "Error: Left CSV not specified. Run the command with the --help" >&2
+	echo "flag for more information." >&2
 	exit 1
 fi
-
 if [ -z "$RIGHT_CSV_PATH" ]; then
-	echo "ERROR: Left *.csv not specified." >&2
+	echo "Error: Right CSV not specified. Run the command with the --help" >&2
+	echo "flag for more information." >&2
 	exit 1
 fi
-
 if [ -z "$LEFT_CSV_COLUMN" ]; then
-	echo "ERROR: Left column number not specified." >&2
+	echo "Error: Left column number not specified. Run the command with the" >&2
+	echo "--help flag for more information." >&2
 	exit 1
 fi
-
 if [ -z "$RIGHT_CSV_COLUMN" ]; then
-	echo "ERROR: Right column number not specified." >&2
+	echo "Error: Right column number not specified. Run the command with the" >&2
+	echo "--help flag for more information." >&2
 	exit 1
 fi
-
 if [ ! -f "$LEFT_CSV_PATH" ]; then
-	echo "ERROR: $LEFT_CSV_PATH does not exist." >&2
+	echo "Error: $LEFT_CSV_PATH does not exist." >&2
 	exit 1
 fi
-
 if [ ! -f "$RIGHT_CSV_PATH" ]; then
-	echo "ERROR: $RIGHT_CSV_PATH does not exist." >&2
+	echo "Error: $RIGHT_CSV_PATH does not exist." >&2
 	exit 1
 fi
 
+# Diff the two columns in the two CSVs:
 diff --new-line-format="" --unchanged-line-format="" \
   <(cut -d, -f$LEFT_CSV_COLUMN "$LEFT_CSV_PATH" | sort | uniq) \
   <(cut -d, -f$RIGHT_CSV_COLUMN "$RIGHT_CSV_PATH" | sort | uniq)
