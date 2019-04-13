1f8a342acad5140db7d5f1f0b933e8e63d62f35c
Zachary Espiritu <me@zacharyespiritu.com>
2019-04-13 18:46:20 -0400

    Update demo-shell.sh

--- demos/demo-shell.sh
+++ demos/demo-shell.sh
@@ -2,6 +2,8 @@
 
 # Creates a "demo shell" for use in demonstrations.
 #
+# The shell used here is /bin/bash.
+#
 # This can be helpful if you want to obfuscate the shell's usual display of the
 # current working directory during a presentation, or just so the prompt looks
 # more professional for demonstration purposes.
@@ -17,9 +19,9 @@ if [ -n "$__IN_DEMO_SHELL__" ]; then
 fi
 export __IN_DEMO_SHELL__="true"
 
-# Note: The $SHELL variable needs to be on the same line as the last assignment
+# Note: The /bin/bash path needs to be on the same line as the last assignment
 # to PROMPT_COMMAND in order for the PROMPT_COMMAND variable to be imported
 # into the shell.
 PROMPT_COMMAND='PS1="\[$(tput setaf 6)\]\[$(tput bold)\][demo] '
 PROMPT_COMMAND+='\[$(tput sgr0)\]\[$(tput setaf 7)\]$ \[$(tput '
-PROMPT_COMMAND+='sgr0)\]"; unset PROMPT_COMMAND;' $SHELL
+PROMPT_COMMAND+='sgr0)\]"; unset PROMPT_COMMAND;' /bin/bash
